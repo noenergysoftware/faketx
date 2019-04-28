33c84f854490a841c412f4947341e0b7b87b11f8
supplient <437566830@qq.com>
2019-04-28 14:10:39 +0800

    Add Back Rob.

--- fakeboy/urls.py
+++ fakeboy/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('TCaptcha.js', views.js),
+    path('ticket/verify', views.verify),
 ]
--- fakeboy/views.py
+++ fakeboy/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def js(request):
@@ -7,3 +7,11 @@ def js(request):
     with open("cheat.js", "rb") as fd:
         data = fd.read()
     return HttpResponse(data)
+
+def verify(request):
+    return JsonResponse(
+        {
+            "response": "1",
+            "err_msg": "ok"
+        }
+    )
